0e219ee252d641f3c273f0e0d1293b847cac0a3b
CritickalGames <zgtalelambdajuegoszg@hotmail.com>
2023-11-16 23:48:00 -0300

    Constantes arregladas parte 3

--- ReadmeGenerator/main.py
+++ ReadmeGenerator/main.py
@@ -4,10 +4,10 @@ import os
 
 #FILEPATH = "../" ya no hace falta; "open" lee apartir de ../ReadmeGenerator
 README= "../README.md"#Quita el "../" cuando hagas el commit
-ACTUALPATH = "ReadmeGenerator/" #Sin esto, no funciona el "open".
-#Pero quita el pat para el commit
-FILENAME_BASE = f"{}config_base.json"
-FILENAME_PROJECTS = f"{}config_projects.json"
+ACTUALPATH = "ReadmeGenerator/../" #Sin esto, no funciona el "open".
+#Pero para el commit agrega el ../ o quita la variable.
+FILENAME_BASE = f"{ACTUALPATH}config_base.json"
+FILENAME_PROJECTS = f"{ACTUALPATH}config_projects.json"
 
 f = open(FILENAME_BASE, "r", errors="ignore", encoding="utf-8")
 data = json.loads(f.read())
